40adb02bd60c72d187548a8676c7f18bac8b06fe
Wiktor <wiktor.markowski81@gmail.com>
2026-06-02 20:56:26 +0200

    Implement a basic RouteType and ConnManager

---
+++ src/conn/mod.rs
@@ -0,0 +1,29 @@
+use axum::{Router, routing::{get, post}};
+use sqlx::{Pool, Sqlite};
+use tokio::net::TcpListener;
+
+
+use crate::{db, routeType::RouteType, routes::users::{CreateUser, GetUsers}};
+
+pub struct ConnManager
+{
+	app:      Router,
+	listener: TcpListener
+}
+
+impl ConnManager
+{
+	pub async fn connect(routes: Vec<RouteType<'_>>) -> Self
+	{
+		let pool = db::ConnectDb().await;
+
+		let app: Router = routes.into_iter().fold(
+			Router::new(),
+			|router, route| router.route(route.path, route.method)
+		).with_state(pool);
+
+		let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+		ConnManager{app, listener}
+	}
+}
