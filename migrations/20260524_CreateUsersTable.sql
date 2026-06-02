a172678aad3d896a8c88ea0c86009668c26e558c
Wiktor <wiktor.markowski81@gmail.com>
2026-06-02 10:12:28 +0200

    Modify migration to be in sqlite

--- migrations/20260524_CreateUsersTable.sql
+++ migrations/20260524_CreateUsersTable.sql
@@ -1,13 +1,6 @@
-CREATE TABLE `users`
+CREATE TABLE users
 (
-	`id`       int          NOT NULL,
-	`username` varchar(255) NOT NULL,
-	`age`      int          NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-ALTER TABLE `users`
-	ADD PRIMARY KEY (`id`),
-	ADD UNIQUE KEY `username` (`username`);
-
-ALTER TABLE `users`
-	MODIFY `id` int NOT NULL AUTO_INCREMENT;
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	username TEXT NOT NULL UNIQUE,
+	age INTEGER NOT NULL
+);
