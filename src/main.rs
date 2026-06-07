82fa3d3821484cf277bd9971015946587d4ce9d2
Wiktor <wiktor.markowski81@gmail.com>
2026-06-07 13:59:44 +0200

    Implement the RouteManager

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use axum::{routing::{get, post}};
 
-use crate::{conn::ConnManager, routeManager::routeType::RouteType, routes::users::{CreateUser, GetUsers}};
+use crate::{conn::ConnManager, routeManager::{RouteManager, routeType::RouteType}, routes::users::{CreateUser, GetUsers}};
 
 mod db;
 mod models;
@@ -12,13 +12,13 @@ mod routeManager;
 async fn main()
 {
 	dotenvy::dotenv().ok();
-		
-	let routes: Vec<RouteType> = vec![
+
+	let routeManager = RouteManager::from(vec![
 		RouteType::new("/users",        get(GetUsers)),
 		RouteType::new("/users/create", post(CreateUser))
-	];
+	]);
 	
-	let conn = ConnManager::connect(routes).await;
+	let conn = ConnManager::connect(routeManager.intoVec()).await;
 	
 	conn.serve().await;
 }
--- src/routeManager/mod.rs
+++ src/routeManager/mod.rs
@@ -1,1 +1,32 @@
+use crate::routeManager::routeType::RouteType;
+
 pub mod routeType;
+
+pub struct RouteManager<'a>
+{
+	routes: Vec<RouteType<'a>>
+}
+
+impl<'a> RouteManager<'a>
+{
+	pub fn new() -> Self
+	{
+		RouteManager{routes: vec![]}
+	}
+
+	pub fn from(routes: Vec<RouteType<'a>>) -> Self
+	{
+		RouteManager{routes}
+	}
+
+	pub fn addRoute(&mut self, route: RouteType<'a>)
+	{
+		self.routes.push(route);
+	}
+
+	// Consumes self
+	pub fn intoVec(self) -> Vec<RouteType<'a>>
+	{
+		self.routes
+	}
+}
